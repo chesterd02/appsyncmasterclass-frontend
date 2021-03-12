9ee7079bcb6493af13e64b10e9003d6ba7397d37
ChesterD02 <chesterd02@gmail.com>
2021-03-12 10:43:33 -0700

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './assets/tailwind.css'
+import Amplify from 'aws-amplify';
+import '@aws-amplify/ui-vue'
+
+Amplify.configure({
+  Auth: {
+    region: 'us-west-1',
+    userPoolId: 'us-west-1_zI8hZwomu',
+    userPoolWebClientId: '1mcb1nqi2rf46pssr7ahu1d0qv',
+    mandatorySignIn: true
+  }
+})
+
+const myAppConfig = {
+  'aws_appsync_graphqlEndpoint' : 'https://ixqnbger4jhinfp34x5m6vp75i.appsync-api.us-west-1.amazonaws.com/graphql',
+  'aws_appsync_region' : 'us-west-1',
+  'aws_appsync_authenticationType': 'AMAZON_COGNITO_USER_POOLS'
+}
+
+Amplify.configure(myAppConfig)
 
 Vue.config.productionTip = false
 
