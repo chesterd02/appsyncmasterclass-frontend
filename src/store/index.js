9f9927c7aac4100da6b7be23298bdc556d880e7b
ChesterD02 <chesterd02@gmail.com>
2021-03-12 12:09:29 -0700

    sync up store

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,12 @@
 import Vue from 'vue';
 import Vuex from  'vuex';
 import authentication from './modules/authentication';
-import signup from './modules/signup';
+// import signup from './modules/signup';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    authentication,
-    signup,
+    authentication
   }
 })
